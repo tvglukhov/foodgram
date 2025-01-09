25b6a6feb264310ebc1ce53717652006eb4b7815
Tim Glukhov <tvglukhov@gmail.com>
2025-01-09 20:58:12 +0500

    Review #2, PATCH bug fixed

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -196,7 +196,6 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         """Валидация запроса на создание Рецепта."""
-        print(data)
         ingredients = data.get('recipe_ingredient')
         tags = data.get('tags')
 
